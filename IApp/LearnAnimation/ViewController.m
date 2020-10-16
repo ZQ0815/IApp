dd36164ace8218fbb6184371a1f1187e321f360a
ZQ0815 <zhangqiang9434@qq.com>
2020-10-16 14:58:47 +0800

    learn: 使用拼接图减少资源加载

--- IApp/LearnAnimation/ViewController.m
+++ IApp/LearnAnimation/ViewController.m
@@ -10,7 +10,7 @@
 
 @interface ViewController ()
 
-@property (nonatomic, weak) IBOutlet UIView *layerView;
+@property (nonatomic, strong) UIView *layerView;
 
 @end
 
@@ -19,15 +19,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //create sublayer
-    self.view.backgroundColor = [UIColor whiteColor];
-    CALayer *blueLayer = [CALayer layer];
-    blueLayer.frame = CGRectMake(125, 388, 200.0f, 200.0f);
+    _layerView = [[UIView alloc] initWithFrame:self.view.frame];
+    _layerView.backgroundColor = [UIColor whiteColor];
+    [self setupCombinationPicture];
+    self.view = _layerView;
+}
+
+- (void)setupCombinationPicture {
     UIImage *image = [UIImage imageNamed:@"p_5"];
-    blueLayer.contents = (__bridge id)image.CGImage;
-    blueLayer.contentsGravity = kCAGravityCenter;
-    blueLayer.contentsScale = [UIScreen mainScreen].scale;
-    blueLayer.masksToBounds = YES;
-    [self.view.layer addSublayer:blueLayer];
+    
+    CALayer *top_left = [CALayer layer];
+    top_left.frame = CGRectMake(0, 100, 50, 50);
+    top_left.contents = (__bridge id)image.CGImage;
+    top_left.contentsGravity = kCAGravityResizeAspect;
+    top_left.contentsRect = CGRectMake(0, 0, 0.5, 0.5);
+    [_layerView.layer addSublayer:top_left];
+    
+    CALayer *top_right = [CALayer layer];
+    top_right.frame = CGRectMake(0, 150, 50, 50);
+    top_right.contents = (__bridge id)image.CGImage;
+    top_right.contentsGravity = kCAGravityResizeAspect;
+    top_right.contentsRect = CGRectMake(0.5, 0, 0.5, 1);
+    [_layerView.layer addSublayer:top_right];
+    
+    CALayer *bottom_left = [CALayer layer];
+    bottom_left.frame = CGRectMake(0, 200, 50, 50);
+    bottom_left.contents = (__bridge id)image.CGImage;
+    bottom_left.contentsGravity = kCAGravityResizeAspect;
+    bottom_left.contentsRect = CGRectMake(0, 0.5, 0.5, 1);
+    [_layerView.layer addSublayer:bottom_left];
+    
+    CALayer *bottom_right = [CALayer layer];
+    bottom_right.frame = CGRectMake(0, 250, 50, 50);
+    bottom_right.contents = (__bridge id)image.CGImage;
+    bottom_right.contentsGravity = kCAGravityResizeAspect;
+    bottom_right.contentsRect = CGRectMake(0.5, 1, 1, 1);
+    [_layerView.layer addSublayer:bottom_right];
 }
 
 @end
